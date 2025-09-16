79fc7e6356e0b6909efc9cc20b7223ecee9250fd
Dasari Sai Yashwanth <116937254+Saiyashwanth7@users.noreply.github.com>
2025-09-16 10:43:10 +0530

    Update post.py

--- src/app/models/post.py
+++ src/app/models/post.py
@@ -2,7 +2,7 @@ import uuid as uuid_pkg
 from datetime import UTC, datetime
 from uuid6 import uuid7
 
-from sqlalchemy import DateTime, ForeignKey, String,UUID
+from sqlalchemy import DateTime, ForeignKey, String, UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
 from ..core.db.database import Base
